1f6f773543c9a4b2a0d46c6249981974b53e58f1
vikyjaware <vikyjaware@gmail.com>
2022-02-10 10:09:34 +0530

    [Viky] Add : Folder structure added

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TicTacToe;
+Console.WriteLine("Welcome to Tic Tac Toe Game !");
+
+TicTacToeGame game=new TicTacToeGame();
+game.Grid();
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+        public void Grid()
+        {
+            char[] grid = new char[9];
+
+            for (int i = 0; i <= 10; i++)
+            {
+                grid[i] = ' ';
+            }
+        }
+    }
+}
